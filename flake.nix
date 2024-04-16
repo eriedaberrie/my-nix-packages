abfb421d03f3eb7ed73b94a256e741ccd53e02d5
eriedaberrie <eriedaberrie@gmail.com>
2024-04-16 09:48:23 -0700

    refactor: use nixpkgs as a flake instead of importing it

--- flake.nix
+++ flake.nix
@@ -5,36 +5,33 @@
 
   outputs = {nixpkgs, ...}: let
     inherit (nixpkgs) lib;
-    forSystems = lib.genAttrs [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-    pkgsFor = system:
-      import nixpkgs {
-        inherit system;
-      };
+    forSystems = f:
+      lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ] (system: f nixpkgs.legacyPackages.${system});
   in {
-    packages = forSystems (system: let
-      pkgs = pkgsFor system;
-    in {
-      fastfetch = pkgs.fastfetch.overrideAttrs (new: (old: {
-        patches = lib.singleton ./pkgs/fastfetch/flashfetch.patch;
-        postPatch =
-          old.postPatch
-          or ""
-          + ''
-            substituteAllInPlace src/flashfetch.c
-          '';
-        flashfetchOptions = "";
-        flashfetchModules = [];
-        flashfetchModulesRaw = lib.concatMapStrings (m: "&options->${m},") new.flashfetchModules;
-      }));
+    packages = forSystems (
+      pkgs: {
+        fastfetch = pkgs.fastfetch.overrideAttrs (new: (old: {
+          patches = lib.singleton ./pkgs/fastfetch/flashfetch.patch;
+          postPatch =
+            old.postPatch
+            or ""
+            + ''
+              substituteAllInPlace src/flashfetch.c
+            '';
+          flashfetchOptions = "";
+          flashfetchModules = [];
+          flashfetchModulesRaw = lib.concatMapStrings (m: "&options->${m},") new.flashfetchModules;
+        }));
 
-      eddie-ui = pkgs.callPackage ./pkgs/eddie-ui {};
-      geticons = pkgs.callPackage ./pkgs/geticons {};
-      syncyomi = pkgs.callPackage ./pkgs/syncyomi {};
-    });
+        eddie-ui = pkgs.callPackage ./pkgs/eddie-ui {};
+        geticons = pkgs.callPackage ./pkgs/geticons {};
+        syncyomi = pkgs.callPackage ./pkgs/syncyomi {};
+      }
+    );
 
-    formatter = forSystems (system: (pkgsFor system).alejandra);
+    formatter = forSystems (pkgs: pkgs.alejandra);
   };
 }
