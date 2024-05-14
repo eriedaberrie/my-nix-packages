d38cadc4fec5c6d9c87aa2bd989d9d0b65cdb214
eriedaberrie <eriedaberrie@gmail.com>
2024-05-14 11:33:16 -0700

    qlot-cli: fix working within other derivations

--- pkgs/qlot-cli/default.nix
+++ pkgs/qlot-cli/default.nix
@@ -8,12 +8,14 @@
   qlot-cli = sbcl.buildASDFSystem rec {
     pname = "qlot";
     version = "1.5.2";
+
     src = fetchFromGitHub {
       owner = "fukamachi";
       repo = "qlot";
-      rev = version;
+      rev = "refs/tags/${version}";
       hash = "sha256-j9iT25Yz9Z6llCKwwiHlVNKLqwuKvY194LrAzXuljsE=";
     };
+
     lispLibs = with sbcl.pkgs; [
       archive
       deflate
@@ -23,29 +25,41 @@
       lparallel
       yason
     ];
+
     nativeLibs = [
       openssl
     ];
+
     nativeBuildInputs = [
       makeWrapper
     ];
+
     buildScript = writeText "build-qlot-cli" ''
       (load "${qlot-cli.asdfFasl}/asdf.${qlot-cli.faslExt}")
       (asdf:load-system :qlot/command)
       (asdf:load-system :qlot/subcommands)
 
-      ;; Use uiop:dump-image instead of sb-ext:dump-image for the image restore hooks
+      ;; Use uiop:dump-image instead of sb-ext:save-lisp-and-die for the image restore hooks
       (setf uiop:*image-entry-point* #'qlot/cli:main)
       (uiop:dump-image "qlot"
                        :executable t
                        #+sb-core-compression :compression
                        #+sb-core-compression t)
     '';
+
     installPhase = ''
+      runHook preInstall
+
       mkdir -p $out/bin
-      mv qlot $out/bin
+      cp qlot.asd $out
+      rm *.asd
+      cp -r * $out
+
+      mv $out/qlot $out/bin
       wrapProgram $out/bin/qlot \
         --prefix LD_LIBRARY_PATH : $LD_LIBRARY_PATH
+
+      runHook postInstall
     '';
   };
 in
