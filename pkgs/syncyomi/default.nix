1335609c1d3be266c5d1efe9eac5c80bb0260d73
eriedaberrie <eriedaberrie@gmail.com>
2024-07-21 23:24:10 -0700

    syncyomi: fix build from stupid buildGoModule inheritance

--- pkgs/syncyomi/default.nix
+++ pkgs/syncyomi/default.nix
@@ -20,38 +20,51 @@ buildGoModule rec {
 
   vendorHash = "sha256-/rpT6SatIZ+GVzmVg6b8Zy32pGybprObotyvEgvdL2w=";
 
-  pnpmRoot = "web";
+  web = stdenvNoCC.mkDerivation (finalAttrs: {
+    pname = "${pname}-web";
+    inherit src version;
+    sourceRoot = "${finalAttrs.src.name}/web";
 
-  pnpmDeps = pnpm_9.fetchDeps {
-    inherit pname version src;
-    sourceRoot = "${src.name}/${pnpmRoot}";
-    hash = "sha256-25Bg8sTeH/w25KdfwgZNoqBXz2d5c1QD5vGb33xpTCA=";
-  };
+    pnpmDeps = pnpm_9.fetchDeps {
+      inherit (finalAttrs) pname version src sourceRoot;
+      hash = "sha256-25Bg8sTeH/w25KdfwgZNoqBXz2d5c1QD5vGb33xpTCA=";
+    };
 
-  nativeBuildInputs = [
-    nodejs
-    pnpm_9.configHook
-  ];
+    nativeBuildInputs = [
+      nodejs
+      pnpm_9.configHook
+    ];
 
-  env.ESBUILD_BINARY_PATH = lib.getExe (esbuild.override {
-    buildGoModule = args:
-      buildGoModule (args
-        // rec {
-          version = "0.17.19";
-          src = fetchFromGitHub {
-            owner = "evanw";
-            repo = "esbuild";
-            rev = "v${version}";
-            hash = "sha256-PLC7OJLSOiDq4OjvrdfCawZPfbfuZix4Waopzrj8qsU=";
-          };
-          vendorHash = "sha256-+BfxCyg0KkDQpHt/wycy/8CTG6YBA/VJvJFhhzUnSiQ=";
-        });
+    env.ESBUILD_BINARY_PATH = lib.getExe (esbuild.override {
+      buildGoModule = args:
+        buildGoModule (args
+          // rec {
+            version = "0.17.19";
+            src = fetchFromGitHub {
+              owner = "evanw";
+              repo = "esbuild";
+              rev = "v${version}";
+              hash = "sha256-PLC7OJLSOiDq4OjvrdfCawZPfbfuZix4Waopzrj8qsU=";
+            };
+            vendorHash = "sha256-+BfxCyg0KkDQpHt/wycy/8CTG6YBA/VJvJFhhzUnSiQ=";
+          });
+    });
+
+    buildPhase = ''
+      runHook preBuild
+      pnpm build
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      cp -r dist $out
+      runHook postInstall
+    '';
   });
 
-  preBuild = ''
-    pushd $pnpmRoot
-    pnpm build
-    popd
+  preConfigure = ''
+    cp -r $web/* web/dist
   '';
 
   ldflags = [
@@ -65,7 +78,7 @@ buildGoModule rec {
   '';
 
   meta = {
-    description = "An open-source project to synchronize Tachiyomi manga reading progress and library across multiple devices";
+    description = "Open-source project to synchronize Tachiyomi manga reading progress and library across multiple devices";
     homepage = "https://github.com/SyncYomi/SyncYomi";
     changelog = "https://github.com/SyncYomi/SyncYomi/releases/tag/v${version}";
     license = lib.licenses.gpl2Only;
