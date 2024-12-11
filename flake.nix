c8f39e948c29b63281a952333f5c63045e9517da
eriedaberrie <eriedaberrie@gmail.com>
2024-12-11 11:21:32 -0800

    eddie-ui: more lazy fix for .NET 6 EOL

--- flake.nix
+++ flake.nix
@@ -22,6 +22,7 @@
             "dotnet-sdk-6.0.428"
             "dotnet-sdk-wrapped-6.0.428"
             "dotnet-runtime-6.0.36"
+            "dotnet-runtime-wrapped-6.0.36"
           ];
         }));
     pins = import ./npins;
